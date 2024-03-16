0a08b3e920daa089d621b73fadd030d829989968
GGB <xtcc02332@naver.com>
2024-03-16 13:48:27 +0900

    modify get-sended/received response json data

--- src/sockets/socket-server.js
+++ src/sockets/socket-server.js
@@ -10,8 +10,8 @@ import { SocketController } from "./controllers/socketController";
 export const SocketServer = async (httpServer) => {
     const io = new Server(httpServer);
     // 
-    // const pubClient = createClient({ legacyMode: false, host: 'localhost', port: 6379});
-    const pubClient = createClient({ legacyMode: false, url: 'redis://redis:6379' });
+    const pubClient = createClient({ legacyMode: false, host: 'localhost', port: 6379});
+    // const pubClient = createClient({ legacyMode: false, url: 'redis://redis:6379' });
     const subClient = pubClient.duplicate();
 
     // Redis 클라이언트에 대한 오류 이벤트 핸들러 추가
