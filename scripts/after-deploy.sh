96821a162b31760c945f1dbb961a859c37274532
ehddn <ehddn205@naver.com>
2024-04-03 19:57:00 +0900

    add action

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -5,6 +5,7 @@ cd $REPOSITORY/api_back
 
 echo "> 🔵 Stop & Remove docker services."
 docker compose down
-
+docker rmi $(docker images -q)
+docker system prune -a -f
 echo "> 🟢 Run new docker services."
 docker compose up --build -d
