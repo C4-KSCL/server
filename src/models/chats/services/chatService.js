2009c750f520fb74ff70bca911b51045a82db4d6
GGB <xtcc02332@naver.com>
2024-04-05 15:18:37 +0900

    commit at 4/5 15:18

--- src/models/chats/services/chatService.js
+++ src/models/chats/services/chatService.js
@@ -102,6 +102,7 @@ export class ChatService {
                     },
                     where: {
                         roomId: join.roomId,
+                        
                     },
                     include: {
                         room: {
