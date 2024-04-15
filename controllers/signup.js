b5f3fdcc07a1a5fd5f676a7449321726cb5a6a30
ehddn <ehddn205@naver.com>
2024-04-15 21:27:51 +0900

    add exception

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,37 +2,46 @@ const nodemailer = require('nodemailer');
 //이메일 인증
 exports.emailauth = (req, res) => {
     const { email } = req.body;
-
-    // 랜덤한 인증번호 생성 (예: 6자리 숫자)
-    const verificationCode = Math.floor(100000 + Math.random() * 900000);
-
-    // 네이버 메일 전송 설정
-    const transporter = nodemailer.createTransport({
-        service: 'Naver',
-        auth: {
-            user: process.env.SEND_MAIL_ID, // 네이버 이메일
-            pass: process.env.SEND_MAIL_PW, // 네이버 비밀번호
-        },
-    });
-
-    // 이메일 전송 옵션
-    const mailOptions = {
-        from: process.env.SEND_MAIL_ID, // 발신자 이메일 주소
-        to: email, // 수신자 이메일 주소
-        subject: '앱이름_인증번호',
-        text: `인증번호: ${verificationCode}`, // 메일 내용은 인증번호
-    };
-
-    // 이메일 전송
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error occurred:', error);
-            return res.status(500).send('이메일 전송에 실패했습니다.');
-        } else {
-            console.log('Email sent:', info.response);
-            // 생성된 랜덤 인증번호를 JSON 형태로 반환
-            return res.status(200).json({ verificationCode });
+    const checkemailQuery = `SELECT * FROM User WHERE email = ?`;
+    req.mysqlConnection.query(checkemailQuery, [email], (err, results) => {
+        if (err) {
+            console.error('Error while querying:', err);
+            return res.status(500).send('서버 에러');
         }
+        if (results.length !== 0) {
+            return res.status(301).send('존재하는 이메일입니다.');
+        }
+
+        // 랜덤한 인증번호 생성 (예: 6자리 숫자)
+        const verificationCode = Math.floor(100000 + Math.random() * 900000);
+        console.log("A")
+        // 네이버 메일 전송 설정
+        const transporter = nodemailer.createTransport({
+            service: 'Naver',
+            auth: {
+                user: process.env.SEND_MAIL_ID, // 네이버 이메일
+                pass: process.env.SEND_MAIL_PW, // 네이버 비밀번호
+            },
+        });
+
+        // 이메일 전송 옵션
+        const mailOptions = {
+            from: process.env.SEND_MAIL_ID, // 발신자 이메일 주소
+            to: email, // 수신자 이메일 주소
+            subject: '앱이름_인증번호',
+            text: `인증번호: ${verificationCode}`, // 메일 내용은 인증번호
+        };
+
+        // 이메일 전송
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error occurred:', error);
+                return res.status(500).send('이메일 전송에 실패했습니다.');
+            } else {
+                // 생성된 랜덤 인증번호를 JSON 형태로 반환
+                return res.status(200).json({ verificationCode });
+            }
+        });
     });
 };
 //닉네임 중복 확인
@@ -53,7 +62,7 @@ exports.checknickname = (req, res) => {
 
 // 회원 DB 등록
 exports.register = (req, res) => {
-    const { email, password, nickname, phoneNumber, age, gender, myMBTI, myKeyword, friendKeyword, friendMBTI,friendMaxAge, friendMinAge, friendGender } = req.body;
+    const { email, password, nickname, phoneNumber, age, gender, myMBTI, myKeyword, friendKeyword, friendMBTI, friendMaxAge, friendMinAge, friendGender } = req.body;
     // 데이터베이스에 사용자 정보 저장
     const checkemailQuery = `SELECT * FROM User WHERE email = ?`;
     req.mysqlConnection.query(checkemailQuery, [email], (err, results) => {
