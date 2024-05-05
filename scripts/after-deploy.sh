cdc3cd50083b31106889c65cdccc34fba3825fb9
ehddn <ehddn205@naver.com>
2024-05-05 15:03:17 +0900

    revise https

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -23,6 +23,6 @@ fi
 # nginx.conf 파일 내의 MY_IP를 .env 파일로부터 읽어온 값으로 대체합니다.
 echo "> 🔵 Replace MY_IP in nginx.conf with the value from .env file."
 sed -i "s/MY_IP/$MY_IP/g" nginx.conf
-sed -i "s/MY_IP/$MY_ADDRESS/g" nginx.conf
+sed -i "s/MY_ADDRESS/$MY_ADDRESS/g" nginx.conf
 echo "> 🟢 Run new docker services."
 docker-compose up --build -d
