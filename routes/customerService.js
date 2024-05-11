5cce54f03201e54c2ba2070774fe8a94a8f46718
ehddn <ehddn205@naver.com>
2024-05-11 16:34:32 +0900

    add customer service and manage

--- routes/customerService.js
+++ routes/customerService.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { verifyAccessToken } = require('../middleware/auth');
-router.post('/post', verifyAccessToken); //게시글 작성 - 일반 사용자
-router.get('/readGeneral', verifyAccessToken); //게시글 조회 - 일반 사용자
-router.get('/readManager', verifyAccessToken); //게시글 조회 - 관리자
-router.post('/responsePost',verifyAccessToken); //답변 작성 - 관리자
+const customerservice = require('../controllers/customerservice');
+router.post('/post', verifyAccessToken, customerservice.imageupload); //게시글 작성 - 일반 사용자
+router.get('/readGeneral', verifyAccessToken, customerservice.readGeneral); //게시글 조회 - 일반 사용자
+router.get('/readManager', verifyAccessToken, customerservice.readManager); //게시글 조회 - 관리자
+router.post('/responsePost',verifyAccessToken, customerservice.responsePost); //답변 작성 - 관리자
 module.exports = router;
