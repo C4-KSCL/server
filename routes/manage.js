46fca2746c8bc96e718f4b224a8225c1cbf1d57d
SanghyunLee <lgw9736@naver.com>
2024-06-05 17:21:56 +0900

    [Fix] searchUser API

--- routes/manage.js
+++ routes/manage.js
@@ -4,5 +4,5 @@ const { verifyAccessToken } = require('../middleware/auth');
 const manage = require('../controllers/manage');
 router.post('/suspend', verifyAccessToken, manage.suspend); //회원 정지 
 router.post('/removeSuspend', verifyAccessToken, manage.removeSuspend); //회원 정지 해제
-router.post('/search', verifyAccessToken, manage.search); //회원 정지 해제
+router.get('/search', verifyAccessToken, manage.search); //회원 검색
 module.exports = router;
