bc6e52caea78bd9d0b3c7a181a8cdc48cf324668
ehddn <ehddn205@naver.com>
2024-06-05 14:47:16 +0900

    add email search

--- routes/manage.js
+++ routes/manage.js
@@ -4,4 +4,5 @@ const { verifyAccessToken } = require('../middleware/auth');
 const manage = require('../controllers/manage');
 router.post('/suspend', verifyAccessToken, manage.suspend); //회원 정지 
 router.post('/removeSuspend', verifyAccessToken, manage.removeSuspend); //회원 정지 해제
+router.post('/search', verifyAccessToken, manage.search); //회원 정지 해제
 module.exports = router;
